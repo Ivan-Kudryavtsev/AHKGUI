94cb75f40c6f1ff6882aee4385662c2d2137920f
Ivan <Ivan.Kudryavtsev@protonmail.com>
2022-07-20 12:23:32 +0100

    Hotkey class fixes and test

--- Main.py
+++ Main.py
@@ -1,23 +1,40 @@
-print("Hello world")
 
 class hotkey:
     # need input key(s), modifiers, output key
     # eventually move on to output functions probably and then parse those but that's too complicated right now
     inputKeys = []
     modifiers = []
-    outputKey = None
+    outputKey = ""
 
     def __init__(self, inputs, output, mods):
-        inputKeys = inputs
-        outputKey = output
-        modifiers = mods
+        self.inputKeys = inputs
+        print(inputs)
+        self.outputKey = output
+        print(output)
+        self.modifiers = mods
+        print(mods)
+
 
     def isValid(self):
-        if (len(self.inputKeys) > 0 and self.outputKey is not None):
+        if (len(self.inputKeys) > 0 and self.outputKey != ""):
             return True
         return False
 
 
+    def output(self):
+        str = "Modifiers: "
+        for mod in self.modifiers:
+            str += mod
+            str += ", "
+        str += "Inputs: "
+        for input in self.inputKeys:
+            str += input
+            str += ", "
+        str += "Output: "
+        str += self.outputKey
+        return str
 
 
+test = hotkey(["A, B, C"], "OMGWTFBBQ", ["Ctrl"])
+print(test.output())
 
