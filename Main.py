932dd6abffb35a314613b28818f618daa7e6eb56
Ivan <Ivan.Kudryavtsev@protonmail.com>
2022-07-20 12:09:26 +0100

    Hotkey class

--- Main.py
+++ Main.py
@@ -1,1 +1,23 @@
 print("Hello world")
+
+class hotkey:
+    # need input key(s), modifiers, output key
+    # eventually move on to output functions probably and then parse those but that's too complicated right now
+    inputKeys = []
+    modifiers = []
+    outputKey = None
+
+    def __init__(self, inputs, output, mods):
+        inputKeys = inputs
+        outputKey = output
+        modifiers = mods
+
+    def isValid(self):
+        if (len(self.inputKeys) > 0 and self.outputKey is not None):
+            return True
+        return False
+
+
+
+
+
